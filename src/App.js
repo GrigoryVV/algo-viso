ae69248820a6e8eefafa21e958e7de7559077847
Grigory Vlasov <54944844+GrigoryVV@users.noreply.github.com>
2020-01-28 21:09:10 +0700

    revert commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from './components/Header/HeaderContainer';
 import SortAlgo from './components/SortAlgo/SortAlgo';
-// import PathAlgo from './components/PathAlgo/PathAlgo';
 import ControlPanelContainer from './components/ControlPanel/ControlPanelContainer';
 import { Route, Switch } from 'react-router-dom';
 
@@ -14,10 +13,10 @@ function App(props) {
       <HeaderContainer/>
       <ControlPanelContainer/>
       <Switch>
-        <Route path="/sorting">
+        <Route path="/algo-viso/sorting">
           <SortAlgo />
         </Route>
-        <Route path="/pathfinding">
+        <Route path="/algo-viso/pathfinding">
           <div className='notificationMSG'>
             Under Construction
           </div>
