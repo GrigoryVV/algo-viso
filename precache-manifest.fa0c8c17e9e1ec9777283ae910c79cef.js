1a1f627aca070547a16961b4aff18aabd90534ed
Grigory Vlasov <54944844+GrigoryVV@users.noreply.github.com>
2020-01-28 20:22:08 +0700

    Updates

---
+++ precache-manifest.fa0c8c17e9e1ec9777283ae910c79cef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e268154dc3e9c4f769bc29b4fb2c051",
+    "url": "/algo-viso/index.html"
+  },
+  {
+    "revision": "1f87586632dfb3e20b9f",
+    "url": "/algo-viso/static/css/main.02066507.chunk.css"
+  },
+  {
+    "revision": "d7edc976609c5be0d33b",
+    "url": "/algo-viso/static/js/2.1ee1d66f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/algo-viso/static/js/2.1ee1d66f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f87586632dfb3e20b9f",
+    "url": "/algo-viso/static/js/main.eac8033a.chunk.js"
+  },
+  {
+    "revision": "d96fbdabe31d4f0bf4c2",
+    "url": "/algo-viso/static/js/runtime-main.90cdce4f.js"
+  }
+]);
